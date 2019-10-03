e2e112a4619fec36ec396a011848c33ba2d0fe0b
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2019-10-03 21:00:45 +0700

    session 2

---
+++ session2.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+	int x;
+	int k;
+	float y;
+	double z;
+	char h;
+	printf("Xin chao T1909E\n");
+	x = 10;
+	k= 5;
+	y = 3.14+5;
+	h= 'a';
+	printf("x =  %d haha\n",x);
+	x = k + 10;
+	printf("x =  %d haha\n",x);
+	x = x + 20;
+	printf("x =  %d haha\n",x);
+	x = x /5;
+	printf("x =  %d haha\n",x);
+	x = x +h;
+	y = x + 30.5;
+	printf("y = %f\n",y);
+	x = (int)(x + 3.14);
+	return 0;
+}
