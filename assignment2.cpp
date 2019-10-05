1bafc4a4659693c19ab1eb39ddb10da87b2c4062
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2019-10-05 19:08:41 +0700

    assignment 2

---
+++ assignment2.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main(){
+	int a,b,c;
+	printf("Nhap so thu nhat:\n");
+	scanf("%d",&a);
+	printf("Nhap so thu 2:\n");
+	scanf("%d",&b);
+	printf("Nhap so thu 3:\n");
+	scanf("%d",&c);
+	if(a > b){
+		if(b > c){
+			printf("Max:  %d -  Min: %d\n",a,c);
+		}else{
+			if(a > c){
+				printf("Max: %d - Min: %d\n",a,b);
+			}else{
+				printf("Max: %d - Min: %d\n",c,b);
+			}
+		}	
+	}else{
+		if(a > c){
+			printf("Max: %d - Min: %d\n",b,c);
+		}else{
+			if(b > c){
+				printf("Max: %d - Min: %d\n",b,a);
+			}else{
+				printf("Max: %d - Min: %d\n",c,a);
+			}
+		}
+	}
+
+	return 0;
+}
