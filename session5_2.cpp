c06935cc5a5ffe581018da24b96294b7ddb0e3c8
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2019-10-12 20:37:39 +0700

    session 5 2

---
+++ session5_2.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+	int n;
+	printf("nhap n=\n");
+	scanf("%d",&n);
+	for (int i = 2; i < n; ++i)
+	{
+		// kiem tra 1 so la nguyen to
+		int dem=0;
+		for(int j=2;j<=i/2;j++){
+			if(i%j==0){
+				dem++;
+				break;
+			}
+		}
+		if(dem == 0){
+			printf("%d\n",i);
+		}
+	}
+	return 0;
+}
